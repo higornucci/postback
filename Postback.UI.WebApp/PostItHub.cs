dacb51942f923a2db72b4cb49b9b3a19a1755131
robsoncastilho <robsoncastilho@yahoo.com.br>
2014-06-06 17:07:03 -0400

    Tratando eventos com SignalR

---
+++ Postback.UI.WebApp/PostItHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Postback.UI.WebApp
+{
+    public class PostItHub : Hub
+    {
+        public void EnviarNovoPostIt(PostItModel postIt)
+        {
+            Clients.All.novoPostIt(postIt.Texto);
+        }
+    }
+
+    public class PostItModel
+    {
+        public string Texto { get; set; }
+    }
+}
---
+++ Postback.UI.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Postback.UI.WebApp.Startup))]
+namespace Postback.UI.WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
