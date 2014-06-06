47429854e1a677434f7faae5a48e8999343c6c06
Guilherme Barbosa Ferreira <gimoteco@gmail.com>
2014-06-06 14:44:27 -0400

    Melhorando as classes de dominio

--- PostBack.Infra/Repositorios/EventoRepositorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using Postback.Dominio;
-
-namespace PostBack.Infra.Repositorios
-{
-    public class EventoRepositorio: RepositorioBase<Evento>
-    {
-         
-    }
-}
--- Postback.Dominio/PostIt.cs
+++ Postback.Dominio/PostIt.cs
@@ -5,5 +5,6 @@
         public string Conteudo { get; set; }
         public Tag Assunto { get; set; }
         public Categoria Categoria { get; set; }
+        public Quadro Quadro { get; set; }
     }
 }
