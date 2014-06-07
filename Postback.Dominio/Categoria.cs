d235a28ddb728d555aaf9486a025d8777865c178
robsoncastilho <robsoncastilho@yahoo.com.br>
2014-06-07 02:04:30 -0400

    Postando com cor da categoria

--- Postback.Dominio/Categoria.cs
+++ Postback.Dominio/Categoria.cs
@@ -1,12 +1,11 @@
 ﻿using Postback.Dominio._Base;
-using System.Drawing;
 
 namespace Postback.Dominio
 {
     public class Categoria : Entidade<Categoria>
     {
         public virtual string Descricao { get; set; }
-        public virtual string CorHexadecimal { get ; set; }
+        public virtual string CorHexadecimal { get; set; }
 
         protected Categoria() { }
 
--- Postback.UI.WebApp/PostItHub.cs
+++ Postback.UI.WebApp/PostItHub.cs
@@ -4,9 +4,9 @@ namespace Postback.UI.WebApp
 {
     public class PostItHub : Hub
     {
-        public void EnviarNovoPostIt(string conteudo, string tagNome, int categoriaId)
+        public void EnviarNovoPostIt(string conteudo, string tagNome, int categoriaId, string categoriaCor)
         {
-            Clients.All.novoPostIt(conteudo, tagNome, categoriaId);
+            Clients.All.novoPostIt(conteudo, tagNome, categoriaId, categoriaCor);
         }
     }
 }
